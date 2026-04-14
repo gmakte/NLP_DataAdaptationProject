8164651ff044bc03675a662a9353df44dc67c7b4
maribelpreite <maribelpreite@gmail.com>
2026-04-14 22:53:57 +0200

    testing LLM generator

---
+++ generate_synth_data.py
@@ -0,0 +1,6 @@
+from transformers import AutoModelForCausalLM, AutoTokenizer
+
+model_name = "mistralai/Mistral-7B-Instruct-v0.3"
+
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+model = AutoModelForCausalLM.from_pretrained(model_name, torch_dtype=torch.float16, device_map="auto")
