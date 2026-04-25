1de8fb502d3a9c6c59e925293f6fcd9d7c39fc9b
gmak <gabija.makutenaite@gmail.com>
2026-04-25 19:12:09 +0200

    more synthetic data gen

--- generate_synth_data.py
+++ generate_synth_data.py
@@ -7,10 +7,12 @@ tokenizer = AutoTokenizer.from_pretrained(model_name)
 
 model = AutoModelForCausalLM.from_pretrained(
     model_name,
-    torch_dtype = torch.bfloat16,
-    device_map = "auto",
-    trust_remote_code = True
+    dtype = torch.bfloat16, #to make memory efficient
+    trust_remote_code = False #to ensure security when loading code from the model repository 
 )
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+model.to(device)
 
 messages = [{
     "role":"user",
@@ -20,3 +22,13 @@ messages = [{
 tokenizer.apply_chat_template(messages, tokenize=False)
 
 model_inputs = tokenizer.apply_chat_template(messages, return_tensors = "pt")
+
+generated_ids = model.generate(
+    model_inputs,
+    max_new_tokens = 500, #to limit the length of generated response
+    do_sample = True,
+)
+
+decoded = tokenizer.batch_decode(generated_ids)
+
+print(decoded[0])
